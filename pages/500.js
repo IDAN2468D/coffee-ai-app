d3551d557763adf2530dac3b6f7b4f003d42c8b6
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2026-01-10 23:15:31 +0200

    Update

---
+++ pages/500.js
@@ -0,0 +1,8 @@
+export default function Custom500() {
+    return (
+        <div style={{ padding: "50px", textAlign: "center" }}>
+            <h1>500 - Server-side error occurred</h1>
+            <p>אירעה שגיאה בשרת. אנחנו עובדים על זה.</p>
+        </div>
+    );
+}
